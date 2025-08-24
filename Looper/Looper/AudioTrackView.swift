173eea35c5bebd29a7fe63d57ac8381ccc4ee3d6
max <liyongteng@icloud.com>
2025-08-24 17:15:33 -0300

    improved recording logic with status control

--- Looper/Looper/AudioTrackView.swift
+++ Looper/Looper/AudioTrackView.swift
@@ -8,23 +8,29 @@ import SwiftUI
 import AVFoundation
 import SwiftData
 
+enum TrackStatus: Int {
+    case idle = 0
+    case recording = 1
+    case playing = 2
+}
+
 struct AudioTrackView: View {
     @Bindable var track: Track
-    @State private var isRecording = false
+    @State private var status: TrackStatus = .idle
     @State private var rotation = 0.0
     @State private var scale = 1.0
-    @State private var playing = false
     @State private var audioRecorder: AVAudioRecorder?
     @State private var recordingURL: URL?
     @State private var audioPlayer: AVAudioPlayer?
+    @Environment(\.scenePhase) private var scenePhase
     let recordingSession: AVAudioSession
     
     var body: some View {
         VStack {
             Button(action: {
-                if playing {
+                if status == .playing {
                     //TODO
-                } else if isRecording {
+                } else if status == .recording {
                     stopRecording()
                 } else {
                     startRecording()
@@ -35,30 +41,44 @@ struct AudioTrackView: View {
                     .scaledToFit()
                     .frame(width: 200, height: 200)
                     .opacity(1.0)
-                    .rotationEffect(.degrees(isRecording ? rotation : 0))
+                    .rotationEffect(.degrees(status == .recording ? rotation : 0))
                     .scaleEffect(scale)
             }
         }
-        .onChange(of: isRecording) {
-            if isRecording {
-                withAnimation(Animation.linear(duration: 2).repeatForever(autoreverses: false)) {
-                    rotation = 360
-                }
-                scale = 1.0
+        .onChange(of: status) {
+            if status == .recording {
+                recordingAnimation()
             } else {
-                withAnimation(.easeInOut(duration: 0.2)) {
-                    rotation = 0
+                playingAnimation()
+            }
+        }
+        .onChange(of: scenePhase) {
+            if scenePhase == .background || scenePhase == .inactive {
+                if status == .recording {
+                    stopRecording()
+                    if let url = recordingURL {
+                        try? FileManager.default.removeItem(at: url)
+                    }
                 }
-                withAnimation(Animation.easeInOut(duration: 0.2).repeatForever(autoreverses: true)) {
-                    scale = 1.1
-                }
+                audioPlayer?.stop()
             }
         }
         .onAppear(){
             if let url = track.url {
                 playAudio(from: url)
+                status = .playing
             }
         }
+        .onDisappear {
+            if status == .recording{
+                stopRecording()
+                if let url = recordingURL {
+                    try? FileManager.default.removeItem(at: url)
+                }
+            }
+            audioPlayer?.stop()
+        }
+
     }
     
     func startRecording() {
@@ -75,19 +95,18 @@ struct AudioTrackView: View {
         do {
             audioRecorder = try AVAudioRecorder(url: audioFileName, settings: settings)
             audioRecorder?.record()
-            isRecording = true
+            status = .recording
             track.setURL(url: audioFileName)
         } catch {
             print("Fail to start recording: \(error.localizedDescription)")
-            isRecording = false
+            status = .idle
         }
     }
     
     func stopRecording() {
         audioRecorder?.stop()
         audioRecorder = nil
-        isRecording = false
-        playing = true
+        status = .playing
         
         if let url = recordingURL {
             playAudio(from: url)
@@ -106,6 +125,22 @@ struct AudioTrackView: View {
             audioPlayer?.play()
         } catch{
             print("Fail to play audio: \(error.localizedDescription)")
+        }
+    }
+    
+    func recordingAnimation() {
+        withAnimation(Animation.linear(duration: 2).repeatForever(autoreverses: false)) {
+            rotation = 360
+        }
+        scale = 1.0
+    }
+    
+    func playingAnimation() {
+        withAnimation(.easeInOut(duration: 0.2)) {
+            rotation = 0
+        }
+        withAnimation(Animation.easeInOut(duration: 0.2).repeatForever(autoreverses: true)) {
+            scale = 1.1
         }
     }
 }
